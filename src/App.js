487a9ebba34d0fe34b1d431dc8ba800f707227e8
NadiiaSka <syvakivska@gmail.com>
2024-02-23 13:21:16 +0700

    Add Loading screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import "./App.css";
-import styled from "styled-components";
+import React from "react";
+import { Html } from "@react-three/drei"; // Import Html component
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Earth } from "./components/earth";
 import Moon from "./components/moon";
 
-const CanvasContainer = styled.div`
-  width: 100%;
-  height: 100%;
-`;
+const LoadingScreen = () => {
+  return (
+    <group>
+      <mesh position={[0, 0, -10]}>
+        <meshBasicMaterial color="white" />
+      </mesh>
+      <Html position={[0, 0, -10]}>
+        <div style={{ color: "white", textAlign: "center", fontSize: "25px" }}>
+          Loading...
+        </div>
+      </Html>
+    </group>
+  );
+};
 
 function App() {
   return (
-    <CanvasContainer>
-      <Canvas>
-        <Suspense fallback={null}>
-          <Earth />
-          <Moon />
-        </Suspense>
-      </Canvas>
-    </CanvasContainer>
+    <Canvas>
+      <Suspense fallback={<LoadingScreen />}>
+        <Earth />
+        <Moon />
+      </Suspense>
+    </Canvas>
   );
 }
 
